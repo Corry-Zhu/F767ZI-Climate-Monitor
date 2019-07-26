57282d6f7aecc2ac8f4fc71b591decdf4603a494
Paul Czeresko <p.czeresko.3@gmail.com>
2019-07-26 12:39:20 -0400

    Implement three Si7021 functions -- see details
    The following functions are written and compiled (but untested):
    Adafruit_Si7021_Init (still needs _readRevision to work)
    Adafruit_Si7021_Begin
    Adafruit_Si7021_Reset
    Adafruit_Si7021_ReadSerialNumber

--- Inc/si7021.h
+++ Inc/si7021.h
@@ -29,6 +29,7 @@
  *
  *  BSD license, all text above must be included in any redistribution
  */
+#include "main.h"
 #include "stm32f7xx_hal.h"
 
 /*!
@@ -36,19 +37,19 @@
  */
 #define SI7021_DEFAULT_ADDRESS	0x40
 
-#define SI7021_MEASRH_HOLD_CMD           0xE5 /**< Measure Relative Humidity, Hold Master Mode */
-#define SI7021_MEASRH_NOHOLD_CMD         0xF5 /**< Measure Relative Humidity, No Hold Master Mode */
-#define SI7021_MEASTEMP_HOLD_CMD         0xE3 /**< Measure Temperature, Hold Master Mode */
-#define SI7021_MEASTEMP_NOHOLD_CMD       0xF3 /**< Measure Temperature, No Hold Master Mode */
-#define SI7021_READPREVTEMP_CMD          0xE0 /**< Read Temperature Value from Previous RH Measurement */
-#define SI7021_RESET_CMD                 0xFE /**< Reset Command */
-#define SI7021_WRITERHT_REG_CMD          0xE6 /**< Write RH/T User Register 1 */
-#define SI7021_READRHT_REG_CMD           0xE7 /**< Read RH/T User Register 1 */
-#define SI7021_WRITEHEATER_REG_CMD       0x51 /**< Write Heater Control Register */
-#define SI7021_READHEATER_REG_CMD        0x11 /**< Read Heater Control Register */
-#define SI7021_ID1_CMD                   0xFA0F /**< Read Electronic ID 1st Byte */
-#define SI7021_ID2_CMD                   0xFCC9 /**< Read Electronic ID 2nd Byte */
-#define SI7021_FIRMVERS_CMD              0x84B8 /**< Read Firmware Revision */
+#define SI7021_MEASRH_HOLD_CMD           0xE5U /**< Measure Relative Humidity, Hold Master Mode */
+#define SI7021_MEASRH_NOHOLD_CMD         0xF5U /**< Measure Relative Humidity, No Hold Master Mode */
+#define SI7021_MEASTEMP_HOLD_CMD         0xE3U /**< Measure Temperature, Hold Master Mode */
+#define SI7021_MEASTEMP_NOHOLD_CMD       0xF3U /**< Measure Temperature, No Hold Master Mode */
+#define SI7021_READPREVTEMP_CMD          0xE0U /**< Read Temperature Value from Previous RH Measurement */
+#define SI7021_RESET_CMD                 0xFEU /**< Reset Command */
+#define SI7021_WRITERHT_REG_CMD          0xE6U /**< Write RH/T User Register 1 */
+#define SI7021_READRHT_REG_CMD           0xE7U /**< Read RH/T User Register 1 */
+#define SI7021_WRITEHEATER_REG_CMD       0x51U /**< Write Heater Control Register */
+#define SI7021_READHEATER_REG_CMD        0x11U /**< Read Heater Control Register */
+#define SI7021_ID1_CMD                   0xFA0FU /**< Read Electronic ID 1st Byte */
+#define SI7021_ID2_CMD                   0xFCC9U /**< Read Electronic ID 2nd Byte */
+#define SI7021_FIRMVERS_CMD              0x84B8U /**< Read Firmware Revision */
 
 #define SI7021_REV_1					0xff  /**< Sensor revision 1 */
 #define SI7021_REV_2					0x20  /**< Sensor revision 2 */
@@ -62,16 +63,6 @@ typedef enum {
 	SI_UNKNOWN,
 } si_sensorType;
 
-_Bool begin();
-float readHumidity();
-float readTemperature();
-si_sensorType getModel();
-uint8_t getRevision();
-void readSerialNumber();
-void reset();
-
-//const static int _TRANSACTION_TIMEOUT = 100; // Wire NAK/Busy timeout in ms
-
 typedef struct __Adafruit_Si7021 {
 	I2C_HandleTypeDef _hi2c;
 	uint32_t sernum_a; /**< Serialnum A */
@@ -80,6 +71,18 @@ typedef struct __Adafruit_Si7021 {
 	uint8_t _revision;
 	int8_t  _i2caddr;
 } Adafruit_Si7021;
+
+_Bool Adafruit_Si7021_Begin(Adafruit_Si7021 *si7021);
+float readHumidity();
+float readTemperature();
+si_sensorType getModel();
+uint8_t getRevision();
+void Adafruit_Si7021_ReadSerialNumber();
+void Adafruit_Si7021_Reset(Adafruit_Si7021 *si7021);
+
+//const static int _TRANSACTION_TIMEOUT = 100; // Wire NAK/Busy timeout in ms
+
+
 
 
 #endif /* SI7021_H_ */
--- Src/si7021.c
+++ Src/si7021.c
@@ -7,13 +7,26 @@
 
 #include "si7021.h"
 
-static uint8_t _readRegister8(uint8_t reg);
+const static uint32_t _TRANSACTION_TIMEOUT = 100; // Wire NAK/Busy timeout in ms
+
+static uint8_t _readRegister8(Adafruit_Si7021 *si7021, uint8_t reg);
 static uint16_t _readRegister16(uint8_t reg);
 static void _writeRegister8(uint8_t reg, uint8_t value);
 static void _readRevision();
 
-static uint8_t _readRegister8(uint8_t reg) {
-	return 0;
+static uint8_t _readRegister8(Adafruit_Si7021 *si7021, uint8_t reg) {
+	uint8_t value;
+	uint8_t cmd = reg;
+
+	if (HAL_I2C_Master_Transmit(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, &cmd, 1, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler(); // TODO: Handle gracefully
+	}
+	HAL_Delay(20);
+	if (HAL_I2C_Master_Receive(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, &value, 1, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler(); // TODO: Handle gracefully
+	}
+
+	return value;
 }
 
 static uint16_t _readRegister16(uint8_t reg) {
@@ -35,7 +48,7 @@ static void _readRevision() {
  *          optional wire object
  */
 void Adafruit_Si7021_Init(Adafruit_Si7021 *si7021, I2C_HandleTypeDef *hi2c) {
-	si7021->_i2caddr = SI7021_DEFAULT_ADDRESS;
+	si7021->_i2caddr = SI7021_DEFAULT_ADDRESS << 1;
 	si7021->_hi2c = *hi2c;
 	si7021->sernum_a = 0; /**< Serialnum A */
 	si7021->sernum_b = 0; /**< Serialnum B */
@@ -48,16 +61,71 @@ void Adafruit_Si7021_Init(Adafruit_Si7021 *si7021, I2C_HandleTypeDef *hi2c) {
  *  @brief  Sets up the HW by reseting It, reading serial number and reading revision.
  *  @return Returns true if set up is successful.
  */
-_Bool begin() {
-
-	// TODO: begin I2C communication
-
-	reset();
-	if (_readRegister8(SI7021_READRHT_REG_CMD) != 0x3A)
+_Bool Adafruit_Si7021_Begin(Adafruit_Si7021 *si7021) {
+	Adafruit_Si7021_Reset(si7021);
+	if (_readRegister8(si7021, SI7021_READRHT_REG_CMD) != 0x3AU)
 		return 0;
 
-	readSerialNumber();
+	Adafruit_Si7021_ReadSerialNumber();
 	_readRevision();
 
 	return 1;
+}
+
+/*!
+ *  @brief  Sends the reset command to Si7021.
+ */
+void Adafruit_Si7021_Reset(Adafruit_Si7021 *si7021) {
+	uint8_t cmd = SI7021_RESET_CMD;
+	if (HAL_I2C_Master_Transmit(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, &cmd, 1, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler();
+	}
+	HAL_Delay(50);
+}
+
+/*!
+ *  @brief  Reads serial number and stores It in sernum_a and sernum_b variable
+ */
+void Adafruit_Si7021_ReadSerialNumber(Adafruit_Si7021 *si7021) {
+	uint8_t cmd[] = {SI7021_ID1_CMD >> 8, SI7021_ID1_CMD & 0xFF};
+	if (HAL_I2C_Master_Transmit(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, cmd, 2, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler();
+	}
+
+	uint8_t sernum[8];
+	if (HAL_I2C_Master_Receive(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, sernum, 8, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler(); // TODO: Handle gracefully
+	}
+
+	si7021->sernum_a = (sernum[0]<<24 | sernum[1]<<16 | sernum[2]<<8 | sernum[3]);
+
+	cmd[0] = SI7021_ID2_CMD >> 8;
+	cmd[1] = SI7021_ID2_CMD & 0xFF;
+	if (HAL_I2C_Master_Transmit(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, cmd, 2, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler();
+	}
+
+	if (HAL_I2C_Master_Receive(&(si7021->_hi2c), (uint16_t)si7021->_i2caddr, sernum, 8, _TRANSACTION_TIMEOUT) != HAL_OK) {
+		Error_Handler(); // TODO: Handle gracefully
+	}
+
+	si7021->sernum_b = (sernum[0]<<24 | sernum[1]<<16 | sernum[2]<<8 | sernum[3]);
+
+	switch(si7021->sernum_b >> 24) {
+	case 0:
+	case 0xFF:
+		si7021->_model = SI_Engineering_Samples;
+		break;
+	case 0x0D:
+		si7021->_model = SI_7013;
+		break;
+	case 0x14:
+		si7021->_model = SI_7020;
+		break;
+	case 0x15:
+		si7021->_model = SI_7021;
+		break;
+	default:
+		si7021->_model = SI_UNKNOWN;
+	}
 }
